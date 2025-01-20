da9d791616d0ac3c8337f8c83b1ffcf4885c2c8e
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2025-01-20 12:56:49 +0900

    :white_check_mark: add test code into test.cs

--- test.cs
+++ test.cs
@@ -1,45 +1,86 @@
+using NUnit.Framework;
+using SecureLibrary;
 using System;
-using System.Runtime.InteropServices;
-using System.Net.Http;
+using System.Text;
 
-namespace HttpHelper
+namespace SecureLibrary.Tests
 {
-    [ComVisible(true)]
-    [Guid("D7691E76-BAD6-4578-A107-6AEFBFF0F458")]
-    [ClassInterface(ClassInterfaceType.None)]
-    public class HttpHelper
+    [TestFixture]
+    public class EncryptionHelperTests
     {
-        public static string HttpGet(string url)
+        private string plainText;
+        private string key;
+        private string password;
+
+        [SetUp]
+        public void Setup()
         {
-            using (HttpClient client = new HttpClient())
-            {
-                try
-                {
-                    var response = client.GetStringAsync(url).Result;
-                    return (response);
-                }
-                catch (Exception ex)
-                {
-                    return ex.Message;
-                }
-            }
+            plainText = "This is a test string";
+            key = EncryptionHelper.KeyGenAES256();
+            password = "securePassword123";
         }
 
-        public static string HttpPost(string url, string data)
+        [Test]
+        public void EncryptAesCbcWithIv_ShouldEncryptAndDecryptSuccessfully()
         {
-            using (HttpClient client = new HttpClient())
-            {
-                try
-                {
-                    var content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
-                    var response = client.PostAsync(url, content).Result.Content.ReadAsStringAsync().Result;
-                    return (response);
-                }
-                catch (Exception ex)
-                {
-                    return ex.Message;
-                }
-            }
+            // Act
+            var encrypted = EncryptionHelper.EncryptAesCbcWithIv(plainText, key);
+            var decrypted = EncryptionHelper.DecryptAesCbcWithIv(encrypted[0], key, encrypted[1]);
+
+            // Assert
+            Assert.AreEqual(plainText, decrypted);
+        }
+
+        [Test]
+        public void KeyGenAES256_ShouldGenerateValidKey()
+        {
+            // Act
+            var generatedKey = EncryptionHelper.KeyGenAES256();
+
+            // Assert
+            Assert.IsNotNull(generatedKey);
+            Assert.AreEqual(44, generatedKey.Length); // Base64 length of 256-bit key
+        }
+
+        [Test]
+        public void GenerateDiffieHellmanKeys_ShouldGenerateKeysSuccessfully()
+        {
+            // Act
+            var keys = EncryptionHelper.GenerateDiffieHellmanKeys();
+
+            // Assert
+            Assert.IsNotNull(keys);
+            Assert.AreEqual(2, keys.Length);
+            Assert.IsNotEmpty(keys[0]); // Public key
+            Assert.IsNotEmpty(keys[1]); // Private key
+        }
+
+        [Test]
+        public void DeriveSharedKey_ShouldDeriveKeySuccessfully()
+        {
+            // Arrange
+            var keys1 = EncryptionHelper.GenerateDiffieHellmanKeys();
+            var keys2 = EncryptionHelper.GenerateDiffieHellmanKeys();
+
+            // Act
+            var sharedKey1 = EncryptionHelper.DeriveSharedKey(keys2[0], keys1[1]);
+            var sharedKey2 = EncryptionHelper.DeriveSharedKey(keys1[0], keys2[1]);
+
+            // Assert
+            Assert.IsNotNull(sharedKey1);
+            Assert.IsNotNull(sharedKey2);
+            Assert.AreEqual(sharedKey1, sharedKey2);
+        }
+
+        [Test]
+        public void BcryptEncoding_ShouldEncodeAndVerifyPasswordSuccessfully()
+        {
+            // Act
+            var hashedPassword = EncryptionHelper.BcryptEncoding(password);
+            var isValid = EncryptionHelper.VerifyBcryptPassword(password, hashedPassword);
+
+            // Assert
+            Assert.IsTrue(isValid);
         }
     }
 }
